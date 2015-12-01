433256115ca75525d79865f2bd7646f3d3653bc7
Jakub Elżbieciak <jelz@post.pl>
2015-12-01 20:04:14 +0100

    Add coverage reporting after successful build

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject codecov-clojure "0.1.0"
   :description "codecov.io, example Clojure project"
   :url "https://github.com/codecov/example-clojure"
+  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.7.0"]])
